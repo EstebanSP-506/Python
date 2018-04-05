7e8910111b34c9018ae9029b0215492fb899bc66
Esteban SP <poesteba@amazon.com>
2018-04-05 16:15:01 -0600

    added django environment and started django apps

---
+++ VirtualEnvironments/Django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\poesteba\docume~1\coding~1\python\virtua~1\django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
