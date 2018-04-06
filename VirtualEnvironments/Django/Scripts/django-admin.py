d25f333f3e140b4a594ddf82d5129da5bc658381
Esteban SP <poesteba@amazon.com>
2018-04-06 17:28:19 -0600

    removed virtualenvironments

--- VirtualEnvironments/Django/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\poesteba\docume~1\coding~1\python\virtua~1\django\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
